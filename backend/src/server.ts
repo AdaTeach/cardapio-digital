887c09a15a3b1bb581064a87191acf72cb928557
i775593 <matheus.passaglia@sap.com>
2026-07-22 12:12:18 -0300

    feat: integrate db initialization in server startup

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,15 +1,18 @@
 import 'dotenv/config';
 import express, { type Request, type Response } from 'express';
+import { getDb } from './db';
 
 const app = express();
 const PORT = process.env.PORT || 3333;
 
 app.use(express.json());
 
-app.get('/', (req: Request, resp: Response) => {
+app.get('/', (_req: Request, resp: Response) => {
     return resp.json({
         data: 'Primeiro retorno'
     });
 })
 
-app.listen(PORT, () => console.log(`funcionando no endereco http://localhost:${PORT}`))
+getDb().then(() => {
+    app.listen(PORT, () => console.log(`funcionando no endereco http://localhost:${PORT}`));
+});
