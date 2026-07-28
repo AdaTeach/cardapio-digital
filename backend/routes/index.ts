208a723d0f37201e39bcdcc92a784f2f56eb6080
Barbara Azevedo <barbara.azevedo@sap.com>
2026-07-28 15:57:54 -0300

    feat(mesa):Adicionada rota POST /mesa em routes/index.ts

--- backend/routes/index.ts
+++ backend/routes/index.ts
@@ -1,6 +1,8 @@
 import { Request, Response, Router } from "express";
 import ProdutoEntity from "../entities/Produto";
 import ProdutoController from "../controller.ts/ProdutoController";
+import MesaEntity from "../entities/Mesa";
+import MesaController from "../controller.ts/MesaController";
 
 const router = Router();
 
@@ -14,6 +16,19 @@ router.post("/produto", async (req: Request, res: Response) => {
     } catch(error) {
         console.log(error);
         res.status(500).json({ error: "Internal server error " });
+    }
+});
+
+router.post("/mesa", async (req: Request, res: Response) => {
+    try {
+        const { data } = req.body;
+        const { numero, capacidade, status } = data;
+        const mesa = new MesaEntity(numero, capacidade, status);
+        const returnMesa = await MesaController.create(mesa);
+        res.status(201).json(returnMesa);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error: "Internal server error" });
     }
 });
 
